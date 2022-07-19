8b9397f4fe6acfe2bc3586c62284ad03f0580d88
Devin Smith <devinsmith@deephaven.io>
2022-07-19 10:22:19 -0700

    Update jpyinterp.sh with new property names

--- jpyinterp.sh
+++ jpyinterp.sh
@@ -13,13 +13,13 @@ function get_property
 TOP_LEVEL_DIR=$PWD
 propertyFile=`find $TOP_LEVEL_DIR -name jpyconfig.properties`
 
-pyLib=`get_property $propertyFile jpy.pythonLib`
+pythonLib=`get_property $propertyFile jpy.pythonLib`
 
 # This is a bit hacky: one assumes that only one copy of jpyLib and jdlLib
 # is available under $TOP_LEVEL_DIR. If it is not the case, one may
 # end up loading the wrong copy.
 # It would be a bit safer to retrieve jpyLib and jdlLib from $propertyFile
-# (like for pyLib). Unfortunately, the automatically generated property file
+# (like for pythonLib). Unfortunately, the automatically generated property file
 # contains paths to build/bdist.<platform>/wheel/ that doesn't exist (it is removed
 # automatically by python setup.py, at least on MacOS).
 # .so files can be found under build/lib.<platform>-<pyversion> not
@@ -29,9 +29,15 @@ jdlLib=`find $TOP_LEVEL_DIR/build -name jdl.*.so -print | head -n 1`
 
 jpyJar=`find target/ -name jpy-*.jar -print | head -n 1`
 
-[ -f "$pyLib" ] || { echo "$0: failed to find pyLib: $pyLib" 1>&2 ; exit 1 ; }
+[ -f "$pythonLib" ] || { echo "$0: failed to find pythonLib: $pythonLib" 1>&2 ; exit 1 ; }
 [ -f "$jpyLib" ] || { echo "$0: failed to find jpyLib: $jpyLib" 1>&2 ; exit 1 ; }
 [ -f "$jdlLib" ] || { echo "$0: failed to find jdlLib: $jdlLib" 1>&2 ; exit 1 ; }
 [ -f "$jpyJar" ] || { echo "$0: failed to find jpyJar: $jpyJar" 1>&2 ; exit 1 ; }
 
-jrunscript -Djpy.pyLib=$pyLib -Djpy.jpyLib=$jpyLib -Djpy.jdlLib=$jdlLib -cp $jpyJar -l python "$@"
+jrunscript \
+  -Dorg.jpy.jsr223.ScriptEngineFactoryImpl.pythonLib=$pythonLib \
+  -Dorg.jpy.jsr223.ScriptEngineFactoryImpl.jpyLib=$jpyLib \
+  -Dorg.jpy.jsr223.ScriptEngineFactoryImpl.jdlLib=$jdlLib \
+  -cp $jpyJar \
+  -l python \
+  "$@"
